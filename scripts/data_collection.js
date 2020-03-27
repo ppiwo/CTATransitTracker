23cb29ffaa29a848114151bd4b7ebbc06bc704ac
patrickpiwowarczyk <piwowarczykpatrick@gmail.com>
2020-03-26 23:08:19 -0500

    GET requests work

--- scripts/data_collection.js
+++ scripts/data_collection.js
@@ -1,33 +1,11 @@
 let route = ["red", "blue", "brown", "green", "orange", "pink", "purple", "yellow"];
 let requestUrl = "https://corsproxy22.herokuapp.com/"; //Using CORS proxy written in Node to prevent CORS errors
-let trainData = []; //Storing all JSON data from API requests
-
-//  async function gatherRequests(){
-//   route.forEach(function(trainLine){
-//     getRequest(trainLine);
-//   });
-// }
-
-
-//     //making api call for each route, appending route color and adding to trainData array
-//      function getRequest (route) {
-//               await fetch(requestUrl + route)
-//               .then(res => res.json())
-//               .then((out) => {
-//                 for (var i = 0; i < out.ctatt.route[0].train.length; i++){ //add route color prop to each train
-//                   out.ctatt.route[0].train[i].lineColor = route;
-//                 }
-//                 trainData.push(out);
-//                 console.log(trainData);
-//                 //plotPoints();
-//               })
-//               .catch(err => { throw err });
-//           };
-
+// let trainData = [""]; //Storing all JSON data from API requests
+console.log('test!');
 
       const getData = (routes)=>{
 
-        let trainmappings = []
+        let trainmappings = [];
 
         const getRes = async (route) =>{
                   await fetch(requestUrl + route)
@@ -37,14 +15,14 @@ let trainData = []; //Storing all JSON data from API requests
                       out.ctatt.route[0].train[i].lineColor = route;
                     }
                     trainmappings.push(out);
-                    console.log(trainData);
+                    console.log(trainmappings);
                     //plotPoints();
                   })
                   .catch(err => { throw err });
               };
 
         routes.map(i => {
-          await getRes(i)
+          getRes(i)
         })
 
 
