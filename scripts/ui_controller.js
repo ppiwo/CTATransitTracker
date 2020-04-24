d2309b95c7db5c2f2127b69ed434739a86316443
patrickpiwowarczyk <piwowarczykpatrick@gmail.com>
2020-04-24 03:33:34 -0500

    rough draft of settings modal complete. Broke data collection.

--- scripts/ui_controller.js
+++ scripts/ui_controller.js
@@ -2,6 +2,17 @@ function trainDetailsToggle(){
 console.log('hi')
 }
 
+
 function settingsToggle(){
-    console.log('bye')
+    var settingsModal = document.getElementById("settingsModal");
+    if (settingsModal.style.display === "none") {
+        settingsModal.style.display = "block";
+    } else {
+        settingsModal.style.display = "none";
+    }
+}
+
+function displayLinesToggle(trainLine){
+    var trainLineIndex = route.indexOf(trainLine);
+    route.splice(trainLineIndex, 1);
 }
